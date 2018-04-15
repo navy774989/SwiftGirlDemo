8680efc3a95d519cf7daad7333beb4b3f9775ec3
Alex Lin <navy774989@gmail.com>
2018-04-15 14:31:43 +0800

    feature: jsonFile

---
+++ SwiftDemo2/SwiftDemo2/FakeData.swift
@@ -0,0 +1,14 @@
+//
+//  FakeData.swift
+//  SwiftDemo2
+//
+//  Created by 林坤興 on 2018/4/15.
+//  Copyright © 2018年 林坤興. All rights reserved.
+//
+
+import UIKit
+
+class FakeData: NSArray {
+
+}
+
